a5b1fd62b05ce784d3b6cd58c9c8ffae7fba9235
Andrew Liu <aeyliu@1000g.sph.umich.edu>
2019-07-12 14:20:56 -0400

    calculating weights

--- scripts/s03-calc-weights.R
+++ scripts/s03-calc-weights.R
@@ -10,11 +10,17 @@ library(MASS)
 args = commandArgs(trailingOnly=TRUE)
 #args[1] is the location of the measured expression
 #args[2] is the directory of predictions
-#args[3] is the target index
-#args[4] is output file location
-#args = vector(length=3)
-#args[1] = "/net/snowwhite/home/aeyliu/pima/prediXcan/gtex-predictions/measured/Cells_EBV-transformed_lymphocytes_Analysis.expr.txt"
-#args[2] = "/net/snowwhite/home/aeyliu/pima/prediXcan/gtex-predictions/predictions/lcl/"
+#args[3] is the target name
+#args[4] is the extracted model information
+#args[5] is index file
+#args[6] is output file location
+
+#args = vector(length=5)
+#args[1] = "/net/snowwhite/home/aeyliu/pima/prediXcan/git/example/Cells_EBV-transformed_lymphocytes_Analysis.expr.txt"
+#args[2] = "/net/snowwhite/home/aeyliu/pima/prediXcan/git/SWAM/test/lcl-test/intermediate/"
+#args[3] = "TW_Cells_EBV-transformed_lymphocytes_0.5_1KG"
+#args[4] = "/net/snowwhite/home/aeyliu/pima/prediXcan/git/SWAM/test/lcl-test/info/"
+#args[5] = "/net/snowwhite/home/aeyliu/pima/prediXcan/git/SWAM/test/lcl-test/index.txt"
 
 inv.norm = function(z)
 {
@@ -28,51 +34,24 @@ inv.norm = function(z)
 ##############################################################
 #process predicted expression
 
-gtex.sample.size = read.csv("/net/snowwhite/home/aeyliu/pima/prediXcan/gtex-sample-sizes.csv",header=T)
-gtex.sample.size = gtex.sample.size[order(as.character(gtex.sample.size[,1])),]
-#omit the gtex tissues that aren't in the weights directory
-names.omit.index = c(7,8,19,20,24,25,31,34,37)
-gtex.info = gtex.sample.size[-names.omit.index,]
+tissue.names = as.character(read.table(args[5],header=F)$V1)
 
-
-file.paths = c('TW_Adipose_Subcutaneous_0.5','TW_Adipose_Visceral_Omentum_0.5','TW_Adrenal_Gland_0.5','TW_Artery_Aorta_0.5','TW_Artery_Coronary_0.5','TW_Artery_Tibial_0.5',
-'TW_Brain_Anterior_cingulate_cortex_BA24_0.5','TW_Brain_Caudate_basal_ganglia_0.5','TW_Brain_Cerebellar_Hemisphere_0.5','TW_Brain_Cerebellum_0.5','TW_Brain_Cortex_0.5',
-'TW_Brain_Frontal_Cortex_BA9_0.5','TW_Brain_Hippocampus_0.5','TW_Brain_Hypothalamus_0.5','TW_Brain_Nucleus_accumbens_basal_ganglia_0.5','TW_Brain_Putamen_basal_ganglia_0.5',
-'TW_Breast_Mammary_Tissue_0.5','TW_Cells_EBV-transformed_lymphocytes_0.5','TW_Cells_Transformed_fibroblasts_0.5','TW_Colon_Sigmoid_0.5','TW_Colon_Transverse_0.5',
-'TW_Esophagus_Gastroesophageal_Junction_0.5','TW_Esophagus_Mucosa_0.5','TW_Esophagus_Muscularis_0.5','TW_Heart_Atrial_Appendage_0.5','TW_Heart_Left_Ventricle_0.5',
-'TW_Liver_0.5','TW_Lung_0.5','TW_Muscle_Skeletal_0.5','TW_Nerve_Tibial_0.5','TW_Ovary_0.5','TW_Pancreas_0.5','TW_Pituitary_0.5','TW_Prostate_0.5',
-'TW_Skin_Not_Sun_Exposed_Suprapubic_0.5','TW_Skin_Sun_Exposed_Lower_leg_0.5','TW_Small_Intestine_Terminal_Ileum_0.5','TW_Spleen_0.5','TW_Stomach_0.5','TW_Testis_0.5',
-'TW_Thyroid_0.5','TW_Uterus_0.5','TW_Vagina_0.5','TW_Whole_Blood_0.5'	)
-
-
-file.paths2 = c('TW_Adipose_Subcutaneous_0.5_1KG','TW_Adipose_Visceral_Omentum_0.5_1KG','TW_Adrenal_Gland_0.5_1KG','TW_Artery_Aorta_0.5_1KG','TW_Artery_Coronary_0.5_1KG','TW_Artery_Tibial_0.5_1KG',
-'TW_Brain_Anterior_cingulate_cortex_BA24_0.5_1KG','TW_Brain_Caudate_basal_ganglia_0.5_1KG','TW_Brain_Cerebellar_Hemisphere_0.5_1KG','TW_Brain_Cerebellum_0.5_1KG','TW_Brain_Cortex_0.5_1KG',
-'TW_Brain_Frontal_Cortex_BA9_0.5_1KG','TW_Brain_Hippocampus_0.5_1KG','TW_Brain_Hypothalamus_0.5_1KG','TW_Brain_Nucleus_accumbens_basal_ganglia_0.5_1KG','TW_Brain_Putamen_basal_ganglia_0.5_1KG',
-'TW_Breast_Mammary_Tissue_0.5_1KG','TW_Cells_EBV-transformed_lymphocytes_0.5_1KG','TW_Cells_Transformed_fibroblasts_0.5_1KG','TW_Colon_Sigmoid_0.5_1KG','TW_Colon_Transverse_0.5_1KG',
-'TW_Esophagus_Gastroesophageal_Junction_0.5_1KG','TW_Esophagus_Mucosa_0.5_1KG','TW_Esophagus_Muscularis_0.5_1KG','TW_Heart_Atrial_Appendage_0.5_1KG','TW_Heart_Left_Ventricle_0.5_1KG',
-'TW_Liver_0.5_1KG','TW_Lung_0.5_1KG','TW_Muscle_Skeletal_0.5_1KG','TW_Nerve_Tibial_0.5_1KG','TW_Ovary_0.5_1KG','TW_Pancreas_0.5_1KG','TW_Pituitary_0.5_1KG','TW_Prostate_0.5_1KG',
-'TW_Skin_Not_Sun_Exposed_Suprapubic_0.5_1KG','TW_Skin_Sun_Exposed_Lower_leg_0.5_1KG','TW_Small_Intestine_Terminal_Ileum_0.5_1KG','TW_Spleen_0.5_1KG','TW_Stomach_0.5_1KG','TW_Testis_0.5_1KG',
-'TW_Thyroid_0.5_1KG','TW_Uterus_0.5_1KG','TW_Vagina_0.5_1KG','TW_Whole_Blood_0.5_1KG'	)
-
-
-
-predicted.expression = vector( mode="list",length=length(file.paths) )
-predicted.extra = vector( mode="list",length=length(file.paths) )
+predicted.expression = vector( mode="list",length=length(tissue.names) )
+predicted.extra = vector( mode="list",length=length(tissue.names) )
 genes.list = NULL
 weights.all=NULL
 
 genes.temp = NULL
 ensg.temp = NULL
 
-for(i in 1:length(file.paths))
+for(i in 1:length(tissue.names))
 {
- print( paste("Processing ", i, " of ", length(file.paths),sep="") )
- #path.pred = paste("/net/snowwhite/home/aeyliu/pima/prediXcan/gtex-predictions/predictions/",file.paths[i],"/predicted_expression.txt",sep="")
- path.pred = paste(args[2],file.paths2[i],"/predicted_expression.txt",sep="")
+ print( paste("Processing ", i, " of ", length(tissue.names),sep="") )
+ path.pred = paste(args[2],tissue.names[i],"/predicted_expression.txt",sep="")
 
  pred.expr = read.table(path.pred,header=TRUE)
  expr.only = pred.expr[,c(-1,-2)]
- path.weights = paste("/net/snowwhite/home/aeyliu/pima/prediXcan/GTEx-V6p-1KG-2016-11-16/",file.paths2[i],".extra.dump",sep="")
+ path.weights = paste(args[4],tissue.names[i],".extra.dump",sep="")
  #path.weights = paste("/net/snowwhite/home/aeyliu/pima/prediXcan/GTEx-hapmap/",file.paths[i],".extra.dump",sep="")
  weights = read.table(path.weights,header=FALSE,sep="|") #weight is column 3 (V3)
  genes.temp = c(genes.temp,as.character(weights$V2))
@@ -85,7 +64,7 @@ for(i in 1:length(file.paths))
  weights = weights[order(as.character(weights$V1)),]
  expr.only = expr.only[,order(names(expr.only))]
  names(expr.only) = as.character(weights$V2)
- #remove duplicates??
+ #remove duplicates
  if(length(which(duplicated(weights$V2))==TRUE)>0)
  {
   expr.only = expr.only[,-which(duplicated(weights$V2)==TRUE)]
@@ -103,7 +82,6 @@ for(i in 1:length(file.paths))
 in.expression = read.table(args[1],header=T)
 
 #need to process it so it's the same format as the predicted expression
-#ensg.map = read.table("/net/snowwhite/home/aeyliu/pima/expr/rnaseq/ensg_map.txt",header=F)
 ensg.map = data.frame(ensg.temp,genes.temp)
 ensg.map = ensg.map[!duplicated(ensg.map),]
 
@@ -135,6 +113,7 @@ in.final = process.measured(in.expression)
 #############
 #set target 
 target.final = in.final
+target.index = which(tissue.names==args[3])
 
 
 #########################################
@@ -172,10 +151,10 @@ calc.tissue.values = function(expr.pred,expr.target)
 
 
 #get value of expression for all tissues
-tissue.values = vector(mode="list",length=length(file.paths))
-for(i in 1:length(file.paths))
+tissue.values = vector(mode="list",length=length(tissue.names))
+for(i in 1:length(tissue.names))
 {
- print(paste("Processing... ", i, " of ", length(file.paths),sep=""))
+ print(paste("Processing... ", i, " of ", length(tissue.names),sep=""))
  tissue.values[[i]] = calc.tissue.values(predicted.expression[[i]],target.final)
 }
 
@@ -183,11 +162,11 @@ for(i in 1:length(file.paths))
 ##########
 #now get the tissue-specific cv prediction accuracy for all tissues
 
-cv.matrix = matrix(nrow=dim(target.final)[1],ncol = length(file.paths))
+cv.matrix = matrix(nrow=dim(target.final)[1],ncol = length(tissue.names))
 
-for(i in 1:length(file.paths))
+for(i in 1:length(tissue.names))
 {
- #print(paste("Processing... ", i, " of ", length(file.paths),sep="")) #no need, it's fast
+ #print(paste("Processing... ", i, " of ", length(tissue.names),sep="")) #no need, it's fast
  genes.temp = rownames(target.final)
  wts.temp = predicted.extra[[i]][,c(2,3)]
  if(sum(duplicated(wts.temp[,1]))>0)
@@ -297,14 +276,13 @@ for(i in 1:dim(tissue.values[[1]])[1])
  {
   print(paste("Processing... ", i, " of ", dim(tissue.values[[1]])[1],sep=""))
  }
- target.weights[i,] = calc.gene.weights2(values.list=tissue.values,target=target.final,index=i,diag.val=3,target.index=args[3])
+ target.weights[i,] = calc.gene.weights2(values.list=tissue.values,target=target.final,index=i,diag.val=3,target.index=target.index)
 }
 
 rownames(target.weights) = rownames(tissue.values[[1]])
 target.weights = replace(target.weights,target.weights<0,0)
 
-write.table(target.weights,args[4],quote=FALSE,sep="\t")
+write.table(target.weights,args[6],quote=FALSE,sep="\t")
 
 
-#write.table(target.weights,"/net/snowwhite/home/aeyliu/pima/prediXcan/calc-weights/jan-2018/lcl.invnorm.diag5.txt",quote=FALSE,sep="\t")
 
