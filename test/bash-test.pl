4cece75f450a32ed5a02fd0e0cee24d9e7fbeddc
Andrew Liu <aeyliu@1000g.sph.umich.edu>
2019-07-16 13:25:28 -0400

    completed script

--- test/bash-test.pl
+++
@@ -1,6 +0,0 @@
-
-
-
-
-
-awk '{print "PrediXcan.py --predict --weights "$2" --dosages genotypes --samples genotypes/samples.txt --output_dir outDir/intermediate/"$1" --linear"}' index.txt | xargs -I {} -P $parallel sh -c \"
